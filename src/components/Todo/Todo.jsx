ab23ce549e5881f71a47fe3cd534a7e26479dfc2
GarbageCode <75023330+GarbageCode1984@users.noreply.github.com>
2023-04-10 13:44:39 +0900

    Update Todo.jsx

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,11 +3,10 @@ import { AiOutlineDelete } from "react-icons/ai";
 import styles from "./Todo.module.css";
 
 const Todo = ({ todo, onUpdate, onDelete }) => {
-  const { id, text, status } = todo; //간편한 사용을 위해 todo에서 할당
+  const { id, text, status } = todo; 
   const handleChange = (e) => {
-    //받아온게 아님 이 컴포넌트 내부에서만 사용
     const status = e.target.checked ? "completed" : "active";
-    onUpdate({ ...todo, status }); //브라우저 Conponents에서 status변하는거 확인!
+    onUpdate({ ...todo, status }); 
   };
   const handleDelete = () => onDelete(todo);
 
